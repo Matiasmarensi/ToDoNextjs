763d42eb31b3e2370eb763a25d27819a14ee7187
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-11-18 14:13:14 -0300

    suspense

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,16 +1,17 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 
 export default function postCard({ post }) {
   return (
     <div>
-      <div>
+      <Link href={`/posts/${post.id}`}>
         <h3>
           {post.id}.{post.title}
         </h3>
-        <p>{post.body}</p>
-        <button onClick={() => {}}></button>
-      </div>
+      </Link>
+      <p>{post.body}</p>
+      <button onClick={() => {}}>Buttom</button>
     </div>
   );
 }
