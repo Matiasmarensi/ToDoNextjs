26d036da56a44682d811c259077e90735ee20a5f
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-11-20 11:42:09 -0300

    deploy

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 
 export default function Notfound() {
   return (
-    <section>
-      <h1>404</h1>
-      <p>Page not found</p>
-      <Link href="/">Go back home</Link>
+    <section className="flex h-[calc(100vh-7rem)] justify-center items-center mt-calc() ">
+      <div className="text-center">
+        <h1 className="text-9xl mb-4">404</h1>
+        <p className="text-2xl mb-4">Page not found</p>
+        <Link className="inline-block bg-blue-700 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-800" href="/">
+          Go back home
+        </Link>
+      </div>
     </section>
   );
 }
